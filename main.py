e27dcbc60c1956ef67ae750b4f17a3f5a0e48739
pavelKuten <90540381+pavelKuten@users.noreply.github.com>
2022-05-15 13:12:43 +0700

    main
    main

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import sys
+from pygame.color import THECOLORS
+
+pygame.init()
+
+
+class Interface:
+    def interface(self):
+        screen = pygame.display.set_mode((1000, 800))
+        screen.fill(THECOLORS['white'])
+
+
+
+
