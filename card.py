fec487333f76c81200d8f6579f545452496c9258
pavelKuten <90540381+pavelKuten@users.noreply.github.com>
2022-05-30 21:12:38 +0700

    Add files via upload

--- card.py
+++ card.py
@@ -2,17 +2,18 @@ import pygame
 
 
 class Card:
-    def __init__(self, img_name, pos):
+    def __init__(self, img_name, pos, index):
         self.img_name = img_name
         self.back_side_img_name = 'img/white.jpg'
         self.img = pygame.image.load(self.img_name)
         self.back_side_img = pygame.image.load(self.back_side_img_name)
-        self.hidden = False
+        self.hidden = True
+        self.deleted = False
         self.pos = pos
+        self.index = index
 
     def draw(self, screen):
-        if self.hidden:
+        if not self.hidden and not self.deleted:
             screen.blit(self.img, self.pos)
-        else:
+        elif not self.deleted:
             screen.blit(self.back_side_img, self.pos)
-
